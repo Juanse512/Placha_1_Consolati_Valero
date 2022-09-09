96bcbf858be947edd89eec6c2b43abe4861343ab
Juan Segundo Valero <51492950+Juanse512@users.noreply.github.com>
2022-09-09 09:15:26 -0300

    Agregado ieee754

--- Ej 9/9.c
+++ Ej 9/9.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ieee754.h>
 
 void get_float(float a){
     unsigned int num = *(unsigned*)&a;
     unsigned int m = (num & 0x007fffff) | (127 << 23);
     float mantissa = *(float*)&m;
     unsigned int e = (num & 0x7f800000) >> 23;
-    printf("Exponente: %d, Mantisa: %f \n", e, mantissa);
+    printf("Funcion Exponente: %d, Mantisa float: %f \n", e, mantissa);
 }
 
 
 int main(){
     float a;
+    union ieee754_float f;
     printf("Ingrese un numero\n");
     scanf("%f", &a);
+    f.f = a;
+    printf("ieee754 exponente: %u mantisa entera: %d\n", f.ieee.exponent, f.iee>
     get_float(a);
     return 0;
 }
